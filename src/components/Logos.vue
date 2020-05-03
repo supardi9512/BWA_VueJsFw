6a4386c1c4959029310fc65df3f8d569d0259bcf
supardi9512 <supardi9512@gmail.com>
2020-05-03 23:23:32 +0700

    mengatur layout

--- src/components/Logos.vue
+++ src/components/Logos.vue
@@ -10,6 +10,18 @@
         <div class="item-logos">
             <img class="logo" alt="Windows 10 logo" src="../assets/windows-10.png">
         </div>
+        <div class="item-logos">
+            <img class="logo" alt="StarBucks logo" src="../assets/starbucks.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo" alt="Google logo" src="../assets/google.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo" alt="Windows 10 logo" src="../assets/windows-10.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo" alt="StarBucks logo" src="../assets/starbucks.png">
+        </div>
     </div>
 </template>
 
@@ -22,18 +34,19 @@ export default {
 <style scoped>
     .title-header {
         margin-bottom: 80px;
+        text-align: left;
     }
 
     .logos-wrapper {
-        width: 70%;
+        width: 66%;
         overflow: hidden;
         margin: auto;
     }
 
     .item-logos {
         float: left;
-        margin-right: 20px;
-        margin-bottom: 20px;
+        margin-right: 70px;
+        margin-bottom: 70px;
         width: 150px;
         height: 150px;
     }
