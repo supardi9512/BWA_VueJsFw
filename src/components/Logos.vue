1ec412afbd5517f72892505754af70f400b6bb50
supardi9512 <supardi9512@gmail.com>
2020-05-03 23:03:01 +0700

    membuat komponen

---
+++ src/components/Logos.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="logos-wrapper">
+        <h2>Find Best Logo</h2>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Logos'
+}
+</script>
+
+<style scoped>
+    .logos-wrapper {
+        width: 70%;
+        overflow: hidden;
+        margin: auto;
+    }
+
+    .item-logos {
+        float: left;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        width: 150px;
+        height: 150px;
+        background-color: red;
+    }
+</style>
